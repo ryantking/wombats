acf1bb65452bbd553e1a5e35286a54d83a6903c9
Ryan King <hello@ryanking.com>
2018-07-25 23:06:22 -0400

    Neaten up build functions and abstract more

--- pkg/ats/build.go
+++ pkg/ats/build.go
@@ -2,6 +2,7 @@ package ats
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -12,41 +13,81 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func checkDir(dir string, lastBuilt time.Time) bool {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Debugf("error reading directory: %s: %s", dir, err)
+		log.Fatalf("could not read directory: %s", dir)
+	}
+
+	for _, f := range files {
+		if f.Mode().IsDir() {
+			newDir := fmt.Sprintf("%s%s/", dir, f.Name())
+			if changed := checkDir(newDir, lastBuilt); changed {
+				return true
+			}
+			continue
+		}
+
+		if strings.Contains(filepath.Ext(f.Name()), "ats") &&
+			f.ModTime().After(lastBuilt) {
+
+			return true
+		}
+	}
+
+	return false
+}
+
 func shouldBuild(execFile string, small bool) bool {
 	execInfo, err := os.Stat(execFile)
 	if err != nil {
 		return true
 	}
 
-	wd, err := os.Getwd()
+	return checkDir("./", execInfo.ModTime())
+}
+
+func removeDuplicates(args []string) []string {
+	seen := map[string]bool{}
+	i := 0
+	for _, arg := range args {
+		if _, ok := seen[arg]; ok {
+			continue
+		}
+		seen[arg] = true
+		args[i] = arg
+		i++
+	}
+
+	return args[:i]
+}
+
+func getArgs(execFile, entryPoint string, clibs []string) []string {
+	args := []string{"-w", "-o", execFile, entryPoint, "-DATS_MEMALLOC_LIBC"}
+	if len(clibs) == 0 {
+		return args
+	}
+
+	pkgCfgArgs := append(clibs, "--cflags", "--libs")
+	cmd := exec.Command("pkg-config", pkgCfgArgs...)
+	cflagsRaw, err := cmd.Output()
 	if err != nil {
-		log.Fatalf("could not get current directory")
+		log.Debugf("pkg-config error: %s", err)
 	}
 
-	files, err := filepath.Glob(filepath.Join(wd, "/*.[dshc]ats"))
-	if err != nil {
-		log.Fatalf("could not find files in directory")
-	}
-	if !small {
-		files2, err := filepath.Glob(filepath.Join(wd, "/**/*.[dshc]ats"))
-		if err != nil {
-			log.Fatalf("could not find files in directory")
-		}
-		files = append(files, files2...)
-	}
+	cflags := strings.Split(strings.TrimSpace(string(cflagsRaw)), " ")
+	return append(args, removeDuplicates(cflags)...)
+}
 
-	for _, file := range files {
-		info, err := os.Stat(file)
-		if err != nil {
-			log.Fatalf("error getting stats on '%s'", file)
-		}
-
-		if info.ModTime().After(execInfo.ModTime()) {
-			return true
-		}
+func calcTime(d time.Duration) string {
+	if d.Hours() >= 1.0 {
+		return fmt.Sprintf("%0.2fh", d.Hours())
+	} else if d.Minutes() >= 1.0 {
+		return fmt.Sprintf("%0.2fm", d.Minutes())
 	}
 
-	return false
+	return fmt.Sprintf("%0.2fs", d.Seconds())
 }
 
 // Build compiles an ATS project
@@ -63,33 +104,14 @@ func Build(name, entryPoint string, clibs []string) string {
 		return execFile
 	}
 
-	args := append(clibs, "--cflags", "--libs")
-	pkgCfg := exec.Command("pkg-config", args...)
-	cflagsRaw, err := pkgCfg.Output()
-	if err != nil {
-		log.Fatalln("could not get clib config")
-	}
-	cflags := strings.Split(strings.TrimSpace(string(cflagsRaw)), " ")
-
 	log.Infof("Building '%s' project", name)
-	patsccArgs := append([]string{"-w", "-o", execFile, entryPoint, "-DATS_MEMALLOC_LIBC"}, cflags...)
-	out, err := ExecPatsccOutput(patsccArgs...)
+	args := getArgs(execFile, entryPoint, clibs)
+	out, err := ExecPatsccOutput(args...)
 	if err != nil {
 		logging.CheckErrors(strings.TrimSpace(out))
 		log.Fatalln("build failed")
 	}
-
-	dur := time.Since(start)
-	unit := "s"
-	t := dur.Seconds()
-	if t >= 60 && t < 3600 {
-		unit = "m"
-		t = dur.Minutes()
-	} else if t >= 36000 {
-		unit = "h"
-		t = dur.Hours()
-	}
-	log.Infof("Finished building in %.2f%s", t, unit)
+	log.Infof("Finished building in %s", calcTime(time.Since(start)))
 
 	return execFile
 }
